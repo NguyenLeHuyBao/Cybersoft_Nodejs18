d34eb036feaa20b28fa458fb1b479488e591a884
ThangNguyen <thotai911999@gmail.com>
2022-01-16 01:50:40 +0700

    refactor code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express");
+const path = require("path");
+
 const { config } = require("./src/config");
 const { rootRoutter } = require("./src/routers/root.router");
-const path = require("path");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const app = express();
-app.use(express.json());
 const port = config.server.port;
 
-//set up static file
+const app = express();
+app.use(express.json());
+
 const pathPublicDirectory = path.join(__dirname, "./public");
-//cho phep duong dan localhost:7000 truy cap truc tiep toi thu muc public
-//localhost:7000/public <==> folder public
+
 app.use("/public", express.static(pathPublicDirectory));
+
 // http://localhost:7000/api/v1
 app.use("/api/v1", rootRoutter);
 
